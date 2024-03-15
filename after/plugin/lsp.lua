089023943a02057bb5653352d0eaba167e74666a
Nathan <nattybe6@gmail.com>
2024-03-15 15:16:58 +0000

    added golang support and a guide to commands

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pyright', 'clangd', 'html', 'cssls', 'eslint', 'tsserver'},
+  ensure_installed = {'pyright', 'clangd', 'html', 'cssls', 'eslint', 'tsserver', 'gopls'},
   handlers = {
     lsp_zero.default_setup,
   },
