2476504da3bcf15fced3e4460da4fdd41a3bd6be
Nathan <nattybe6@gmail.com>
2024-03-15 16:21:55 +0000

    updated keymaps

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,4 +1,10 @@
 require("nvim-tree").setup()
 local api = require("nvim-tree.api")
-vim.keymap.set("n", "<leader>pv", api.tree.open) 
 vim.keymap.set("n", "<leader>t", api.node.open.tab) 
+vim.keymap.set("n", "<leader>pv", api.tree.toggle) 
+vim.keymap.set("n","<leader><Right>", "gt")
+vim.keymap.set("n","<leader><Left>", "gT")
+vim.keymap.set("n","<leader>d", ":noh")
+
+
+
--- lua/nathan/remap.lua
+++ lua/nathan/remap.lua
@@ -1,4 +1,2 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n","<leader><Right>", "gt")
-vim.keymap.set("n","<leader><Left>", "gT")
