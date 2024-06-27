50dea01fefa96b028123378d04afe4a9d0f341ca
Nathan <nattybe6@gmail.com>
2024-06-27 21:20:48 +0100

    added tailwind lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pyright', 'clangd', 'html', 'cssls', 'eslint', 'tsserver', 'gopls'},
+  ensure_installed = {'pyright', 'clangd', 'html', 'cssls', 'eslint', 'tsserver', 'gopls', 'tailwindcss'},
   handlers = {
     lsp_zero.default_setup,
   },
